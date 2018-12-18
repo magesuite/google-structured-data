f988557482f4a8e8867bb6aad99839929993cd0e
Blazej Doleska <blazej.doleska@creativestyle.pl>
2018-12-18 16:02:12 +0100

    added comment to not clear implementation

--- Plugin/RemoveSchemaInBlocks.php
+++ Plugin/RemoveSchemaInBlocks.php
@@ -5,6 +5,9 @@ class RemoveSchemaInBlocks
 {
     public function aroundGetData(\Magento\Framework\View\Element\Template $subject, callable $proceed, $key = '', $index = null)
     {
+        /**
+         * Logic is added to avoid duplicating Magento Schema.org data with data provided in this module
+         */
         if ($key != 'schema') {
             return $proceed($key, $index);
         }
