c91b078029a6aa035462ebfce163c0bd15bd945c
Dariusz Matoga <dariusz.matoga@creativestyle.pl>
2022-04-07 11:40:42 +0000

    feat: Improve loading products associated to grouped

---
+++ Plugin/GroupedProduct/Model/Product/Type/Grouped/AddAttributesToAssociatedProducts.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace MageSuite\GoogleStructuredData\Plugin\GroupedProduct\Model\Product\Type\Grouped;
+
+class AddAttributesToAssociatedProducts
+{
+    protected $attributesToSelect;
+
+    /**
+     * @var \MageSuite\GoogleStructuredData\Provider\Data\Product\CompositeAttribute
+     */
+    protected $compositeAttributeDataProvider;
+
+    public function __construct(
+        \MageSuite\GoogleStructuredData\Provider\Data\Product\CompositeAttribute $compositeAttributeDataProvider,
+        array $attributesToSelect = []
+    ) {
+        $this->compositeAttributeDataProvider = $compositeAttributeDataProvider;
+        $this->attributesToSelect = $attributesToSelect;
+    }
+
+    public function afterGetAssociatedProductCollection(\Magento\GroupedProduct\Model\Product\Type\Grouped $subject, $result)
+    {
+        $attributesToSelect = array_filter(array_values($this->compositeAttributeDataProvider->getEavAttributeCodes()));
+        $attributesToSelect = array_merge($this->attributesToSelect, $attributesToSelect);
+
+        $result->addAttributeToSelect($attributesToSelect);
+
+        return $result;
+    }
+}
