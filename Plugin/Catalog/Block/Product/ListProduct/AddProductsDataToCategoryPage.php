92f0514a1416cfb063d82c45fdc060690bb2b488
Paweł Ledwig <pawel.ledwig@creativestyle.pl>
2023-03-29 15:06:52 +0200

    fix: [TOYS-737] Performance optimization. Check if customer is present at category page.

--- Plugin/Catalog/Block/Product/ListProduct/AddProductsDataToCategoryPage.php
+++ Plugin/Catalog/Block/Product/ListProduct/AddProductsDataToCategoryPage.php
@@ -5,28 +5,35 @@ namespace MageSuite\GoogleStructuredData\Plugin\Catalog\Block\Product\ListProduc
 class AddProductsDataToCategoryPage
 {
     protected \Magento\Framework\DataObjectFactory $dataObjectFactory;
-
     protected \MageSuite\GoogleStructuredData\Helper\Configuration $configuration;
-
     protected \MageSuite\GoogleStructuredData\Provider\StructuredDataContainer $structuredDataContainer;
-
     protected \MageSuite\GoogleStructuredData\Provider\Data\Product $productDataProvider;
+    protected \Magento\Framework\Registry $registry;
 
     public function __construct(
         \Magento\Framework\DataObjectFactory $dataObjectFactory,
         \MageSuite\GoogleStructuredData\Helper\Configuration $configuration,
         \MageSuite\GoogleStructuredData\Provider\StructuredDataContainer $structuredDataContainer,
-        \MageSuite\GoogleStructuredData\Provider\Data\Product $productDataProvider
+        \MageSuite\GoogleStructuredData\Provider\Data\Product $productDataProvider,
+        \Magento\Framework\Registry $registry
     ) {
         $this->configuration = $configuration;
         $this->structuredDataContainer = $structuredDataContainer;
         $this->productDataProvider = $productDataProvider;
         $this->dataObjectFactory = $dataObjectFactory;
+        $this->registry = $registry;
     }
 
     public function afterGetLoadedProductCollection(\Magento\Catalog\Block\Product\ListProduct $subject, $result)
     {
         if (!$this->configuration->isCategoryPageIncludeProducts()) {
+            return $result;
+        }
+
+        /** @var \Magento\Catalog\Model\Category|null $currentCategory */
+        $currentCategory = $this->registry->registry('current_category');
+
+        if (!isset($currentCategory) || !$currentCategory->getId()) {
             return $result;
         }
 
