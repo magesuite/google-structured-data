7855ec03fc5b0016af13a5ca7fa5dba057c24ea0
Dariusz Matoga <dariusz.matoga@creativestyle.pl>
2023-05-02 09:12:28 +0200

    feat: [IPET-1998] Add configuration for displaying reviews on POP

--- Plugin/Catalog/Block/Product/ListProduct/AddProductsDataToCategoryPage.php
+++ Plugin/Catalog/Block/Product/ListProduct/AddProductsDataToCategoryPage.php
@@ -5,14 +5,14 @@ namespace MageSuite\GoogleStructuredData\Plugin\Catalog\Block\Product\ListProduc
 class AddProductsDataToCategoryPage
 {
     protected \Magento\Framework\DataObjectFactory $dataObjectFactory;
-    protected \MageSuite\GoogleStructuredData\Helper\Configuration $configuration;
+    protected \MageSuite\GoogleStructuredData\Helper\Configuration\Category $configuration;
     protected \MageSuite\GoogleStructuredData\Provider\StructuredDataContainer $structuredDataContainer;
     protected \MageSuite\GoogleStructuredData\Provider\Data\Product $productDataProvider;
     protected \Magento\Framework\Registry $registry;
 
     public function __construct(
         \Magento\Framework\DataObjectFactory $dataObjectFactory,
-        \MageSuite\GoogleStructuredData\Helper\Configuration $configuration,
+        \MageSuite\GoogleStructuredData\Helper\Configuration\Category $configuration,
         \MageSuite\GoogleStructuredData\Provider\StructuredDataContainer $structuredDataContainer,
         \MageSuite\GoogleStructuredData\Provider\Data\Product $productDataProvider,
         \Magento\Framework\Registry $registry
@@ -42,10 +42,11 @@ class AddProductsDataToCategoryPage
         }
 
         $i = 0;
+        $isShowRating = $this->configuration->isShowRating();
 
         $result->addMediaGalleryData();
         foreach ($result as $product) {
-            $productData = $this->productDataProvider->execute($product, false);
+            $productData = $this->productDataProvider->execute($product, $isShowRating);
 
             $productDataObject = $this->dataObjectFactory->create();
             $productDataObject->setData($productData);
