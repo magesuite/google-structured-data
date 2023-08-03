0685c4ccb8753580c2db0047ff77317a5fa0410d
Dariusz Matoga <dariusz.matoga@creativestyle.pl>
2023-08-03 07:15:35 +0000

    feat: [IPET-2120] Add possibility to skip products associated to grouped product

--- Provider/Data/FaqPage/Question.php
+++ Provider/Data/FaqPage/Question.php
@@ -6,20 +6,20 @@ namespace MageSuite\GoogleStructuredData\Provider\Data\FaqPage;
 class Question
 {
     protected string $question;
-    
+
     protected string $answer;
-    
+
     public function __construct(string $question, string $answer)
     {
         $this->question = $question;
         $this->answer = $answer;
     }
-    
+
     public function getQuestion(): string
     {
         return $this->question;
     }
-    
+
     public function getAnswer(): string
     {
         return $this->answer;
