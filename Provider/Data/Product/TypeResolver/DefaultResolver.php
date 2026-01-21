7ca957219d222b109ffda134223ac2ed0270b1d3
Piotr Matras <piotr.matras@creativestyle.pl>
2026-01-21 10:02:26 +0100

    feat: HAAR-5147 add product param to audience method

--- Provider/Data/Product/TypeResolver/DefaultResolver.php
+++ Provider/Data/Product/TypeResolver/DefaultResolver.php
@@ -35,7 +35,7 @@ class DefaultResolver implements \MageSuite\GoogleStructuredData\Provider\Data\P
         $productData = $this->getBaseProductData($product, $store);
         $offerData = $this->getOffers($product, $store);
         $reviewsData = $this->getReviewsData($product, $store);
-        $audienceData = $this->getAudienceData($store);
+        $audienceData = $this->getAudienceData($product, $store);
 
         return array_merge($productData, $offerData, $reviewsData, $audienceData);
     }
@@ -160,7 +160,10 @@ class DefaultResolver implements \MageSuite\GoogleStructuredData\Provider\Data\P
         return $row;
     }
 
-    public function getAudienceData(\Magento\Store\Api\Data\StoreInterface $store): array
+    public function getAudienceData(
+        \Magento\Catalog\Api\Data\ProductInterface $product,
+        \Magento\Store\Api\Data\StoreInterface $store
+    ): array
     {
         if (!$this->productConfiguration->isAudienceEnabled((int)$store->getId())) {
             return [];
