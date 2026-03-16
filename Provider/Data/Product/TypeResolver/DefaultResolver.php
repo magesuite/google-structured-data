fc2652e825c4e6f2d73bd4cd505bbf9d8c4b9a65
Paweł Złamaniec <pawel.zlamaniec@creativestyle.pl>
2026-03-16 12:07:39 +0000

    Revert "Merge branch 'fix/KRG-3400/use-store-url-for-images' into '3.x'"
    This reverts merge request !90

--- Provider/Data/Product/TypeResolver/DefaultResolver.php
+++ Provider/Data/Product/TypeResolver/DefaultResolver.php
@@ -20,8 +20,7 @@ class DefaultResolver implements \MageSuite\GoogleStructuredData\Provider\Data\P
         protected \MageSuite\GoogleStructuredData\Model\Review\GetProductReviews $getProductReviews,
         protected \MageSuite\GoogleStructuredData\Model\Review\GetProductRattingSummary $getProductRattingSummary,
         protected \MageSuite\GoogleStructuredData\Helper\Configuration\Product $productConfiguration,
-        protected \MageSuite\GoogleStructuredData\Model\ResourceModel\Product\InventoryData $inventoryData,
-        protected \Magento\Store\Model\App\Emulation $appEmulation
+        protected \MageSuite\GoogleStructuredData\Model\ResourceModel\Product\InventoryData $inventoryData
     ) {}
 
     public function isApplicable(string $productTypeId): bool
@@ -140,10 +139,7 @@ class DefaultResolver implements \MageSuite\GoogleStructuredData\Provider\Data\P
 
     public function getProductImages(\Magento\Catalog\Api\Data\ProductInterface $product): array
     {
-        $storeId = (int)$product->getStoreId();
-        $this->appEmulation->startEnvironmentEmulation($storeId, \Magento\Framework\App\Area::AREA_FRONTEND, true);
         $mediaGallery = $product->getMediaGalleryImages();
-        $this->appEmulation->stopEnvironmentEmulation();
 
         if (!is_array($mediaGallery->getItems())) {
             return [];
